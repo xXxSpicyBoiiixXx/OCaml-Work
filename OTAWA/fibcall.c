e3065b8ce2de59a66b2ebedbeee448a4c1976944
Md Ali <mdali251@yahoo.com>
2021-05-21 01:22:51 -0500

    Fibcalls

--- OTAWA/fibcall.c
+++ OTAWA/fibcall.c
@@ -1,3 +1,23 @@
 int fib(int n) {
 	int i, Fnew, Fold, temp, ans; 
 	
+	Fnew = 1; Fold = 0;
+	i = 2; 
+	
+	while(i <= n)	
+	{ 
+		temp = Fnew; 
+		Fnew = Fnew + Fold;
+		Fold = temp;
+		i++;
+	}
+
+	ans = Fnew; 
+	return ans;
+}
+
+main() {
+	int a; 
+	a = 30; 
+	fib(a);
+}
