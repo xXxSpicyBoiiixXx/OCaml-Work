89f47d10c1701d1cd49c937755d53d7a1751869e
xXxSpicyBoiiixXx <mdali251@yahoo.com>
2021-07-03 20:45:29 -0500

    failed example file

---
+++ OTAWA/ex-computation/example-context.c
@@ -0,0 +1,19 @@
+int t = 0; 
+
+void f(int n) {
+	for(int i = 0; i < n; i++) 
+		/* block 1*/ 
+
+	for(int i = 0; i < n && t < 100; i++) {
+		/*block 2*/ 
+		t = t + 1;
+	}
+}
+
+int main void { 
+
+	f(80);
+
+	f(80);
+
+}
